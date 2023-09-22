0c1d622ef25da67cc45cea5d1ca1786ad3aebd09
puang59 <kk61468@gmail.com>
2023-09-23 03:42:31 +0530

    random2

--- logic.py
+++ logic.py
@@ -7,13 +7,8 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.gridlayout import GridLayout
 from kivy.lang import Builder
 from kivymd.app import MDApp
-<<<<<<< HEAD
-
-import requests
-=======
 from kivy.core.window import Window
 Window.size = (350, 600)
->>>>>>> 25938b2fa056afaa51aa34c58d8c6a58e30e8329
 import tweepy
 import re
 
@@ -53,41 +48,7 @@ Builder.load_file("home.kv")
 Builder.load_file("twitter.kv")
 Builder.load_file("twitterresult.kv")
 class HomeScreen(Screen):
-<<<<<<< HEAD
-
     pass
-    '''def __init__(self, **kwargs):
-        super(HomeScreen, self).__init__(**kwargs)
-        boxlayout = BoxLayout(orientation='vertical', spacing=0,spac)
-        header = Label(text="Social Media Fake Profile Detection", font_size='24sp', bold=True, height=0)
-        label = Label(text="Select a Social Media Platform:", height=30)
-
-        layout = GridLayout(cols=2, spacing=10)
-
-        twitter_button = Button(text="Twitter")
-        twitter_button.bind(on_release=self.open_twitter_screen)
-
-        instagram_button = Button(text="Instagram")
-        instagram_button.bind(on_release=self.open_instagram_screen)
-
-        linkedin_button = Button(text="LinkedIn")
-        linkedin_button.bind(on_release=self.open_linkedin_screen)
-
-        facebook_button = Button(text="Facebook")
-        facebook_button.bind(on_release=self.open_facebook_screen)
-
-        boxlayout.add_widget(header)
-        boxlayout.add_widget(label)
-        boxlayout.add_widget(layout)
-        layout.add_widget(twitter_button)
-        layout.add_widget(instagram_button)
-        layout.add_widget(linkedin_button)
-        layout.add_widget(facebook_button)
-
-        self.add_widget(boxlayout)'''
-=======
-    pass
->>>>>>> 25938b2fa056afaa51aa34c58d8c6a58e30e8329
 
     def open_twitter_screen(self):
         self.manager.current = 'twitter'
@@ -116,7 +77,7 @@ class TwitterScreen(Screen):
         layout.add_widget(button)
         layout.add_widget(back_button)
         self.add_widget(layout)'''
-    
+
 
     def get_profile_info(self,twitter_url):
         # Extract the Twitter username from the URL
@@ -134,32 +95,11 @@ class TwitterScreen(Screen):
         else:
             print("Invalid Twitter URL")
 
-
     def bckBtn(self, instance):
         self.manager.current = 'home'
 
-<<<<<<< HEAD
-class TwitterResult(Screen):
-    def __init__(self, **kwargs):
-        super(TwitterResult, self).__init__(**kwargs)
-        layout = BoxLayout(orientation='vertical')
-        self.result_label = Label(text="Twitter Profile Info Will Be Shown Here")
-
-        detect_button = Button(text="Detect", background_color=(0, 1, 0, 1))
-        detect_button.bind(on_release=self.detect)
-
-        back_button = Button(text="Back")
-        back_button.bind(on_release=self.bckBtn)
-
-        layout.add_widget(self.result_label)
-        layout.add_widget(detect_button)
-        layout.add_widget(back_button)
-
-        self.add_widget(layout)
-=======
 class Twitterresult(Screen):
     result_label = None  # Define result_label as a class attribute
->>>>>>> 25938b2fa056afaa51aa34c58d8c6a58e30e8329
 
     def on_pre_enter(self):
         profile_info = App.get_running_app().profile_info
@@ -185,83 +125,7 @@ class Twitterresult(Screen):
         self.manager.current = 'twitter'
 
     def detect(self, instance):
-        self.manager.current = 'twitter_detect'
-
-class TwitterDetect(Screen):
-    def __init__(self, **kwargs):
-        super(TwitterDetect, self).__init__(**kwargs)
-        layout = BoxLayout(orientation='vertical')
-        self.result_label = Label(text="Result")
-
-        block_button = Button(text="Block", background_color=(1, 0, 0, 1))
-        block_button.bind(on_release=self.block)
-
-        back_button = Button(text="Back")
-        back_button.bind(on_release=self.bckBtn)
-
-        layout.add_widget(self.result_label)
-        layout.add_widget(block_button)
-        layout.add_widget(back_button)
-
-        self.add_widget(layout)
-
-    def on_pre_enter(self):
-        profile_info = App.get_running_app().profile_info
-        if profile_info:
-            # Extract and display important information
-            user_name = profile_info.get('name', '')
-            screen_name = profile_info.get('screen_name', '')
-            followers_count = profile_info.get('followers_count', 0)
-            friends_count = profile_info.get('friends_count', 0)
-            statuses_count = profile_info.get('statuses_count', 0)
-
-            info_text = f"Name: {user_name}\n"
-            info_text += f"Username: {screen_name}\n"
-            info_text += f"Followers: {followers_count}\n"
-            info_text += f"Friends: {friends_count}\n"
-            info_text += f"Statuses: {statuses_count}\n"
-
-            self.result_label.text = info_text
-
-    def bckBtn(self, instance):
-        self.manager.current = 'twitter_result'
-
-    def block(self, instance):
-        profile_info = App.get_running_app().profile_info
-        screen_name = profile_info.get('screen_name', '')
-        try:
-            # Get the user's ID using their screen name
-            user = api.get_user(screen_name=screen_name)  # Provide screen_name as a keyword argument
-            user_id = user.id_str
-
-            # Block the user by their user_id
-            api.create_block(user_id=user_id)  # Pass user_id as a keyword argument
-
-            print(f"User @{screen_name} has been blocked successfully.")
-        except tweepy.TweepyException as e:
-            print(f"Error blocking user @{screen_name}: {e.reason}")
-
-    # def block(self, instance):
-    #     profile_info = App.get_running_app().profile_info
-    #     twitter_id = profile_info.get('id', 0)
-    #     print(f"THIS IS TWITTER ID: {twitter_id}")
-
-    #     url = f"https://api.twitter.com/2/users/:{twitter_id}/blocking"
-    #     headers = {
-    #         "Authorization": f"Bearer {config.bearer_token}"
-    #     }
-    #     data = {
-    #         "target_user_id": twitter_id
-    #     }
-    #     response = requests.post(url, headers=headers, json=data)
-
-    #     if response.status_code == 201:
-    #         print("User blocked successfully.")
-    #     elif response.status_code == 404:
-    #         error_message = response.json().get('errors', [{}])[0].get('message', 'User not found.')
-    #         print(f"Error blocking user: {error_message}")
-    #     else:
-    #         print(f"Error blocking user. Status code: {response.status_code}, Response: {response.text}")
+        pass
 
 
 class InstagramScreen(Screen):
@@ -479,23 +343,16 @@ class MyApp(MDApp):
     profile_info = None
 
     def build(self):
-        self.title='VeriScan'
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "DeepPurple"
         sm = ScreenManager()
         home_screen = HomeScreen(name= 'home')
         sm.add_widget(home_screen)
 
-<<<<<<< HEAD
-        sm.add_widget(TwitterScreen(name='twitter'))
-        sm.add_widget(TwitterResult(name='twitter_result'))
-        sm.add_widget(TwitterDetect(name='twitter_detect'))
-=======
         twitter = TwitterScreen(name='twitter')
         sm.add_widget(twitter)
-        
+
         sm.add_widget(Twitterresult(name='twitter_result'))
->>>>>>> 25938b2fa056afaa51aa34c58d8c6a58e30e8329
 
         sm.add_widget(InstagramScreen(name='instagram'))
         sm.add_widget(InstagramResult(name='instagram_result'))
